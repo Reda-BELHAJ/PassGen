b59733c52e8f05672d8eab064e249f610c84680f
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-17 13:38:19 +0100

    Updates

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+        <>
+            <header className="fixed w-full border-t-4 bg-gray-100 dark:bg-gray-900 border-green-500 dark:border-green-600 shadow dark:shadow-2 z-50">
+                
+            </header>
+        </>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <>
+      <Navbar />
     </>
   )
 }
