c5815786716d18df04543521a1fbd08a7be6c590
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2021-07-17 13:18:09 +0100

    Add files via upload

---
+++ passwordstrength.py
@@ -0,0 +1,9 @@
+import re
+v=input("Enter the password:")
+if(len(v)>=8):
+    if(bool(re.match('((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,30})',v))==True):
+        print("The password is strong")
+    elif(bool(re.match('((\d*)([a-z]*)([A-Z]*)([!@#$%^&*]*).{8,30})',v))==True):
+        print("The password is weak")
+else:
+    print("You have entered an invalid password.")
---
+++ passwordstrengthvalid.py
@@ -0,0 +1,23 @@
+import re
+p= input("Input your password")
+x = True
+while x:  
+    if (len(p)<6 or len(p)>12):
+        break
+    elif not re.search("[a-z]",p):
+        break
+    elif not re.search("[0-9]",p):
+        break
+    elif not re.search("[A-Z]",p):
+        break
+    elif not re.search("[$#@]",p):
+        break
+    elif re.search("\s",p):
+        break
+    else:
+        print("Valid Password")
+        x=False
+        break
+
+if x:
+    print("Not a Valid Password ")
