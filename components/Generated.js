bbfe62f87a1a9a2316f53725b868ad1b48f3a89b
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-17 21:33:50 +0100

    CopyToClipboard Funct

--- components/Generated.js
+++ components/Generated.js
@@ -1,30 +1,32 @@
+import React from 'react';
 import {MdContentCopy} from 'react-icons/md'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-const Generated = () => {
-    const password = 'Nl6v2V*9FW%7sdoO4&c0kUiAgR%'
+export default class Generated extends React.Component {
+    password = 'Nl6v2V*9FW%7sdoO4&c0kUiAgR%'
+
     state = {
-        value: '',
+        value: this.password,
         copied: false,
       };
 
-    return (
-        <>
-            <div className="relative p-6 my-4 w-full rounded border-b-4 border-red-500 dark:border-red-800 bg-gray-100 dark:bg-gray-800">
-                <div className="font-bold text-gray-800 dark:text-gray-100 mr-4 overflow-hidden" >
-                    {password}
+      render() {
+          return (
+            <>
+                <div className="relative p-6 my-4 w-full rounded border-b-4 border-red-500 dark:border-red-800 bg-gray-100 dark:bg-gray-800">
+                    <div className="font-bold text-gray-800 dark:text-gray-100 mr-4 overflow-hidden" >
+                        {this.password}
+                    </div>
+                    
+                    <CopyToClipboard  
+                        text={this.state.value}
+                        onCopy={() => this.setState({copied: true})}>
+                            <button className="font-bold absolute w-5 h-10 text-gray-400 right-3 top-3 dark:text-gray-300" >
+                                <MdContentCopy />
+                            </button>
+                    </CopyToClipboard >
                 </div>
-                
-                <CopyToClipboard  
-                    text={this.state.value}
-                    onCopy={() => this.setState({copied: true})}>
-                        <button className="font-bold absolute w-5 h-10 text-gray-400 right-3 top-3 dark:text-gray-300" >
-                            <MdContentCopy />
-                        </button>
-                </CopyToClipboard >
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
-
-export default Generated
